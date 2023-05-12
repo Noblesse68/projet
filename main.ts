56bc44742d1fde2292b2a26f342ed5c6294d6e2e
Noblesse68 <132678684+Noblesse68@users.noreply.github.com>
2023-05-12 15:30:11 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
+let t = 0
+input.onButtonPressed(Button.A, function () {
+    t = randint(1, 3)
+    if (t == 1) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    } else {
+        if (t == 2) {
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+        } else {
+            if (t == 3) {
+                basic.showLeds(`
+                    # # # # #
+                    # . . . #
+                    # . . . #
+                    # . . . #
+                    # # # # #
+                    `)
+            } else {
+            	
+            }
+        }
+    }
+})
 basic.forever(function () {
 	
 })
